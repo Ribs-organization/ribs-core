80af991c147f154fc30dc3b7f0f6449288002576
Piou-piou <pilloud.anthony@gmail.com>
2017-12-21 18:45:12 +0100

    [func] adding function do a toggleSlide like jquery function

--- source/js/RibsCore.js
+++ source/js/RibsCore.js
@@ -13,6 +13,28 @@ class RibsCore {
 
     return height;
   }
+
+  /**
+   * @param element
+   * @param duration
+   * this method do animation on height when displaying an element
+   * if max height = none it show the element else this function hide it
+   */
+  static toggleSlide(element, duration) {
+    let maxHeight = 0;
+
+    if (window.getComputedStyle(element).getPropertyValue('max-height') === 'none') {
+      maxHeight = this.getHeight(element);
+    }
+
+    element.style.transition = 'max-height 0.5s ease-in-out';
+    element.style.maxHeight = 0;
+    element.style.display = 'block';
+
+    setTimeout(function() {
+      element.style.maxHeight = `${maxHeight}px`;
+    }, duration)
+  }
 }
 
 export default (RibsCore);
