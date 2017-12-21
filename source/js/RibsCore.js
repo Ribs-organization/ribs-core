5a1b4e6a0309ce9904abc93744aa2229c6699b29
Piou-piou <pilloud.anthony@gmail.com>
2017-12-21 18:36:58 +0100

    [func] adding function to get height of an element if it is hidden or not

---
+++ source/js/RibsCore.js
@@ -0,0 +1,16 @@
+import 'babel-polyfill';
+
+class RibsCore {
+  /**
+   * @param element
+   * @returns {Number}
+   * function that return height of an element that is no displayed
+   */
+  static getHeight(element) {
+    element.style.display = 'block';
+    let height = parseInt(window.getComputedStyle(element).getPropertyValue('height'));
+    element.style.display = '';
+
+    return height;
+  }
+}
