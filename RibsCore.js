6014cba61992dc21b34599be178da89ad4337da0
AnthonyPilloud <anthony.pilloud@quarks.pro>
2019-08-09 07:29:11 +0200

    [func] add method to validate a mail address

--- RibsCore.js
+++ RibsCore.js
@@ -101,6 +101,17 @@ class RibsCore {
 
     return null;
   }
+
+  /**
+   * method to validate a mail address
+   * @param mail
+   * @returns {boolean}
+   */
+  static validateMail(mail) {
+    const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return regex.test(mail);
+  }
 }
 
 export default (RibsCore);
